94af34c0da3d2802ab8e1e7aa633f1f9c1358ccd
信徒 <chenzw@chenzwdeimac.local>
2018-11-22 14:59:17 +0800

    Initial Commit

---
+++ Codes/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Codes
+//
+//  Created by chenzw on 2018/11/22.
+//  Copyright © 2018 Gripay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Codes/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  Codes
+//
+//  Created by chenzw on 2018/11/22.
+//  Copyright © 2018 Gripay. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
